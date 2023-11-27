a8fdd36d4ed4f2bb5a7119d6a7d1d23cc0779568
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-11-27 16:57:55 -0600

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/app/app.component.html"],
+  theme: {
+    extend: {
+      colors: {
+        backgroundColor: 'rgb(253 224 71)',
+      },
+    },
+  },
+  plugins: [],
+}
+
