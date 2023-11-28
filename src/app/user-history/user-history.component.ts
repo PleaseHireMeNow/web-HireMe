2d89b0210383d14357ba65780255bd6035f8dbf4
Gabe Glasco <gabeglasco@gmail.com>
2023-11-28 14:35:11 -0600

    add components with boilerplate

---
+++ src/app/user-history/user-history.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-history',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-history.component.html',
+  styleUrl: './user-history.component.scss'
+})
+export class UserHistoryComponent {
+
+}
---
+++ src/app/user-progress/user-progress.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-progress',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-progress.component.html',
+  styleUrl: './user-progress.component.scss'
+})
+export class UserProgressComponent {
+
+}
