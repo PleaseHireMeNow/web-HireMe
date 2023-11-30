0aee67e4cc5650684204848f8701210fd53e3b23
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-11-30 11:07:53 -0600

    made some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuestionAnswerComponent } from './components/question-answer/question-answer.component';
+import { UserHistoryComponent } from './components/user-history/user-history.component';
+import { UserProgressComponent } from './components/user-progress/user-progress.component';
+
+const AppRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'question', component: QuestionAnswerComponent },
+    { path: 'history', component: UserHistoryComponent },
+    { path: 'progress', component: UserProgressComponent },
+    { path: '**', redirectTo: '/home' }
+];
+
+export default AppRoutes
