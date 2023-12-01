3bc8b839c935600fe26118434c751ddb7a47c8d1
Gabe Glasco <gabeglasco@gmail.com>
2023-12-01 12:40:34 -0600

    add correct yellows global style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: { },
+    extend: { 
+      colors: {
+      'sunglow': {
+          '50': '#fefbe8',
+          '100': '#fff8c2',
+          '200': '#ffee87',
+          '300': '#ffdd43',
+          '400': '#ffcd2a',
+          '500': '#efae03',
+          '600': '#ce8500',
+          '700': '#a45e04',
+          '800': '#88490b',
+          '900': '#733c10',
+          '950': '#431e05',
+      },  
+    },  
   },
+},
   plugins: [],
 }
 
