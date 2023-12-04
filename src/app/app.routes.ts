08ec00b00cbbc03a3d746fa97e13163de9795f75
Gabrielle Glasco <116385698+glascog@users.noreply.github.com>
2023-12-04 12:00:29 -0600

    add user component and import user history and progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { QuestionAnswerComponent } from './components/question-answer/question-answer.component';
-import { UserHistoryComponent } from './components/user-history/user-history.component';
-import { UserProgressComponent } from './components/user-progress/user-progress.component';
+import { UserHistoryComponent } from './components/user/user-history/user-history.component';
+import { UserProgressComponent } from './components/user/user-progress/user-progress.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { UserComponent } from './components/user/user.component';
 
 const AppRoutes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -14,6 +15,7 @@ const AppRoutes: Routes = [
     { path: 'question', component: QuestionAnswerComponent },
     { path: 'history', component: UserHistoryComponent },
     { path: 'progress', component: UserProgressComponent },
+    { path: 'user', component: UserComponent },
     { path: '**', redirectTo: '/home' }
 ];
 
