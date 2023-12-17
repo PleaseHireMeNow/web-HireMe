2d7f76a9f7856a094d1435696f825eda5c7acff5
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-12-16 21:47:55 -0600

    captured the click events

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -8,68 +8,75 @@ import { Question } from '../../common/models/question';
 
 const emptyTopic: Topics = {
   name: '',
-  iconPath: ''
-}
+  iconPath: '',
+};
 
 const emptyDifficulty: Difficulty = {
   name: '',
   iconPath: '',
-}
-
-
+};
 
 @Component({
   selector: 'app-topics',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './topics.component.html',
-  styleUrl: './topics.component.scss'
+  styleUrl: './topics.component.scss',
 })
 export class TopicsComponent {
-
-  questions: Question[] = []; 
+  questions: Question[] = [];
   topics: Topics[] = [];
   difficulty: Difficulty[] = [];
   selectedTopic = emptyTopic;
   selectedDifficulty = emptyDifficulty;
 
-  constructor(private apiService: QuestionService) {}; 
-  ngOnInit(): void {   
-    this.apiService.getAllQuestions().subscribe((data: any) => {     
+  constructor(private apiService: QuestionService) {}
+  ngOnInit(): void {
+    this.apiService.getAllQuestions().subscribe((data: any) => {
       this.questions = data;
       console.log('data:', data);
     });
 
-    this.apiService.getAllTopics().subscribe((data: any) => {     
+    this.apiService.getAllTopics().subscribe((data: any) => {
       this.topics = data.topics;
-      this.difficulty = data.difficulty
+      this.difficulty = data.difficulty;
       console.log('topics:', data.topics[0].name);
-      console.log('topics1:',this.topics)
-      console.log('difficulty:',this.difficulty)
+      console.log('topics1:', this.topics);
+      console.log('difficulty:', this.difficulty);
     });
   }
 
-  selectTopic(topic: any){
-    console.log('In the selectTopic')
+  selectTopic(topic: any) {
+    console.log('In the selectTopic');
     this.selectedTopic = topic;
-    console.log('the selected topic:', this.selectedTopic)
+    console.log('the selected topic:', this.selectedTopic);
   }
 
-  selectDifficulty(level:any){
-    console.log('In the diffectly')
+  selectDifficulty(level: any) {
+    console.log('In the diffectly');
     this.selectedDifficulty = level;
-    console.log('Selected Difficulty',this.selectedDifficulty)
+    console.log('Selected Difficulty', this.selectedDifficulty);
   }
 
-  handleSubmit(){
-    console.log('I Have been clicked!', this.selectedTopic.name, this.selectedDifficulty.name);
-    if(this.selectedTopic.name !== '' && this.selectedDifficulty.name  !== ''){
-      console.log(' I CAN MOVE ON')
-    }
-    else{
-      console.log('WRONGGGGGGG CAN"T MOVE ON')
-      alert('Please pick a topic or difficulty')
+  handleSubmit() {
+    console.log(
+      'I Have been clicked!',
+      this.selectedTopic.name,
+      this.selectedDifficulty.name
+    );
+    if (this.selectedTopic.name !== '' && this.selectedDifficulty.name !== '') {
+      console.log(' I CAN MOVE ON');
+      let send = {};
+      const sendTo = [
+        (send = {
+          topic: this.selectedTopic,
+          difficulty: this.selectedDifficulty,
+        }),
+      ];
+      console.log('send this,', sendTo)
+    } else {
+      console.log('WRONGGGGGGG CAN"T MOVE ON');
+      alert('Please pick a topic or difficulty');
     }
   }
-
 }
