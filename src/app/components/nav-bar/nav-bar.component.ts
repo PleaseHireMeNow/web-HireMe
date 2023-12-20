6326e9079cae6fdea93dd80ebb8a3074f09191ac
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-12-20 11:13:33 -0600

    add a settings

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,5 +10,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+
+  constructor(private router: Router) {}
+
+  handleSetting(){
+    console.log('Setting has been clicked')
+    this.router.navigate(['/topics'])
+
+  }
 
 }
