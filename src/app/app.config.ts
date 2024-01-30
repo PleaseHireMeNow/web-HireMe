f341b7a79e1b0f87d3d416ff2c60d9ac4c0936fc
Gabrielle Glasco <116385698+glascog@users.noreply.github.com>
2024-01-30 14:40:44 -0600

    add google signin setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,33 @@ import { provideRouter } from '@angular/router';
 import  AppRoutes  from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter (AppRoutes), 
               provideHttpClient(),
-              provideOAuthClient()
+              provideOAuthClient(),
+              {
+                provide: 'SocialAuthServiceConfig',
+                useValue: {
+                  autoLogin: false,
+                  providers: [
+                    {
+                      id: GoogleLoginProvider.PROVIDER_ID,
+                      provider: new GoogleLoginProvider(
+                        '747480745644-js0b8j9stspu814lbtctegsa9jioj7kt.apps.googleusercontent.com'
+                      )
+                    }
+                  ],
+                  onError: (error) => {
+                    console.error(error);
+                  }
+                } as SocialAuthServiceConfig, }
   ]
 };
+
